6628025ef520aaa6211cf3363306f3ca11ec567a
hk <hkkim2709@gmail.com>
2025-04-15 14:01:21 +0900

    매도매수 TC 추가

--- TradingSystem/TradingSystem/main.cpp
+++ TradingSystem/TradingSystem/main.cpp
@@ -1,5 +1,6 @@
 #include "IStockTrader.h"
 #include "gmock/gmock.h"
+#include <string>
 
 IStockTrader *selectStockerBrocker(const string &driver) {
   if (driver == "Kiwer") return new KiwerDriver();
@@ -12,13 +13,33 @@ TEST(Basic, SelectSBKiwer) {
   EXPECT_EQ("Kiwer", stockTrader->getType());
 }
 
-// TEST(Basic, Login_Pass) { EXPECT_EQ(true, SbdInterface().login(id,
-// password)); }
+TEST(Basic, Login_Pass) {
+    EXPECT_EQ(true, SbdInterface().LogIn(id, password));
+}
 
-// TEST(Basic, Login_Fail) {
-//   EXPECT_EQ(false, SbdInterface().login(id, password));  // wrong id
-//   EXPECT_EQ(false, SbdInterface().login(id, password));  // wrong password
-// }
+TEST(Basic, Login_Fail) {
+    EXPECT_EQ(false, SbdInterface().LogIn(id, password));  // wrong id
+    EXPECT_EQ(false, SbdInterface().LogIn(id, password));  // wrong password
+}
+
+TEST(Basic, BuySell) {
+    std::string stockCode = 0;
+
+    // buy test
+    int buyPrice = 100;
+    int buyCount = 10;
+    EXPECT_EQ(true, SbdInterface().buyStock(stockCode, buyPrice, buyCount));
+
+    EXPECT_EQ(buyCount, SbdInterface().getStockCount(stockCode));
+
+    // sell test
+    int sellPrice = 100;
+    int sellCount = 5;
+
+    EXPECT_EQ(true, SbdInterface().sellStock(stockCode, sellPrice, sellCount));
+
+    EXPECT_EQ((buyCount - sellCount), SbdInterface().getStockCount(stockCode));
+}
 
 int main() {
   ::testing::InitGoogleMock();
