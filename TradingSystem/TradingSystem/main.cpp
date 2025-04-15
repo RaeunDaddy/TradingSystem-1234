02d7a23c7f8c15677fb362c928f2f3494dd6b83f
hk <hkkim2709@gmail.com>
2025-04-15 13:35:22 +0900

    TC 추가 - select, login

--- TradingSystem/TradingSystem/main.cpp
+++ TradingSystem/TradingSystem/main.cpp
@@ -1,6 +1,19 @@
 #include "gmock/gmock.h"
 
+TEST(Basic, SelectSB) { 
+    EXPECT_EQ(true, SbdInterface().selectStockBrocker(stockBrocker));
+}
+
+TEST(Basic, Login_Pass) {
+    EXPECT_EQ(true, SbdInterface().login(id, password));
+}
+
+TEST(Basic, Login_Fail) {
+    EXPECT_EQ(false, SbdInterface().login(id, password));   // wrong id
+    EXPECT_EQ(false, SbdInterface().login(id, password));   // wrong password
+}
+
 int main() {
-	::testing::InitGoogleMock();
-	return RUN_ALL_TESTS();
+    ::testing::InitGoogleMock();
+    return RUN_ALL_TESTS();
 }
