3cf9541537553a9bd9837b8654c63cacae227c18
Chanwook Lee <cksdnr1@gmail.com>
2025-04-15 14:11:00 +0900

    feature selectStocerBrocker added

--- TradingSystem/TradingSystem/IStockTrader.h
+++ TradingSystem/TradingSystem/IStockTrader.h
@@ -5,9 +5,32 @@ using std::string;
 
 #define interface struct
 
-interface IStockTrader{
-	virtual void LogIn(const string & id, const string & passwd) = 0;
-	virtual void buyStock(const string& stockCode, int buyingPrice, int quantity) = 0;
-	virtual void sellStock(const string& stockCode, int sellingPrice, int quantity) = 0;
-	virtual void getStockPrice(const string& stockCode) = 0;
+interface IStockTrader {
+  virtual void LogIn(const string& id, const string& passwd) = 0;
+  virtual void buyStock(const string& stockCode, int buyingPrice,
+                        int quantity) = 0;
+  virtual void sellStock(const string& stockCode, int sellingPrice,
+                         int quantity) = 0;
+  virtual void getStockPrice(const string& stockCode) = 0;
+  virtual string getType() = 0;
+};
+
+class KiwerDriver : public IStockTrader {
+  void LogIn(const string& id, const string& passwd) override {};
+  void buyStock(const string& stockCode, int buyingPrice,
+                int quantity) override {};
+  void sellStock(const string& stockCode, int sellingPrice,
+                 int quantity) override {};
+  void getStockPrice(const string& stockCode) override {};
+  string getType() override { return "Kiwer"; }
+};
+
+class NemoDriver : public IStockTrader {
+  void LogIn(const string& id, const string& passwd) override {};
+  void buyStock(const string& stockCode, int buyingPrice,
+                int quantity) override {};
+  void sellStock(const string& stockCode, int sellingPrice,
+                 int quantity) override {};
+  void getStockPrice(const string& stockCode) override {};
+  string getType() override { return "Nemo"; }
 };
--- TradingSystem/TradingSystem/main.cpp
+++ TradingSystem/TradingSystem/main.cpp
@@ -1,19 +1,26 @@
+#include "IStockTrader.h"
 #include "gmock/gmock.h"
 
-TEST(Basic, SelectSB) { 
-    EXPECT_EQ(true, SbdInterface().selectStockBrocker(stockBrocker));
+IStockTrader *selectStockerBrocker(const string &driver) {
+  if (driver == "Kiwer") return new KiwerDriver();
+  if (driver == "Nemo") return new NemoDriver();
+  return nullptr;
 }
 
-TEST(Basic, Login_Pass) {
-    EXPECT_EQ(true, SbdInterface().login(id, password));
+TEST(Basic, SelectSBKiwer) {
+  IStockTrader *stockTrader = selectStockerBrocker("Kiwer");
+  EXPECT_EQ("Kiwer", stockTrader->getType());
 }
 
-TEST(Basic, Login_Fail) {
-    EXPECT_EQ(false, SbdInterface().login(id, password));   // wrong id
-    EXPECT_EQ(false, SbdInterface().login(id, password));   // wrong password
-}
+// TEST(Basic, Login_Pass) { EXPECT_EQ(true, SbdInterface().login(id,
+// password)); }
+
+// TEST(Basic, Login_Fail) {
+//   EXPECT_EQ(false, SbdInterface().login(id, password));  // wrong id
+//   EXPECT_EQ(false, SbdInterface().login(id, password));  // wrong password
+// }
 
 int main() {
-    ::testing::InitGoogleMock();
-    return RUN_ALL_TESTS();
+  ::testing::InitGoogleMock();
+  return RUN_ALL_TESTS();
 }
